6d8551a6a00082cf98031f9c8edf0d3b37215313
tenstormavi <avi.avinash3008@gmail.com>
2016-04-29 23:26:39 +0530

    Navigation drawer implement

--- EMedi/gen/com/example/emedi/R.java
+++ EMedi/gen/com/example/emedi/R.java
@@ -20,44 +20,63 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int navigation_drawer_width=0x7f040002;
     }
     public static final class drawable {
-        public static final int emedi=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int mybutton=0x7f020002;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int emedi=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int mybutton=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int con_pass=0x7f080006;
-        public static final int login=0x7f080003;
-        public static final int password=0x7f080001;
-        public static final int reg_pass=0x7f080007;
-        public static final int reg_user=0x7f080009;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080004;
-        public static final int textView3=0x7f080005;
-        public static final int user_reg=0x7f080008;
-        public static final int username=0x7f080002;
+        public static final int action_settings=0x7f08000e;
+        public static final int con_pass=0x7f080009;
+        public static final int container=0x7f080001;
+        public static final int drawer_layout=0x7f080000;
+        public static final int login=0x7f080006;
+        public static final int navigation_drawer=0x7f080002;
+        public static final int password=0x7f080004;
+        public static final int reg_pass=0x7f08000a;
+        public static final int reg_user=0x7f08000c;
+        public static final int section_label=0x7f08000d;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080008;
+        public static final int user_reg=0x7f08000b;
+        public static final int username=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_list_options=0x7f030000;
+        public static final int activity_drawer_options=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_signup=0x7f030002;
         public static final int activity_splash_page=0x7f030003;
+        public static final int fragment_drawer_contact=0x7f030004;
+        public static final int fragment_drawer_demo=0x7f030005;
+        public static final int fragment_navigation_drawer=0x7f030006;
     }
     public static final class menu {
-        public static final int list_options=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int signup=0x7f070002;
-        public static final int splash_page=0x7f070003;
+        public static final int drawer_options=0x7f070000;
+        public static final int global=0x7f070001;
+        public static final int list_options=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int signup=0x7f070004;
+        public static final int splash_page=0x7f070005;
     }
     public static final class string {
+        public static final int action_example=0x7f05000c;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int navigation_drawer_close=0x7f05000b;
+        public static final int navigation_drawer_open=0x7f05000a;
+        public static final int title_activity_drawer_options=0x7f050006;
         public static final int title_activity_list_options=0x7f050004;
         public static final int title_activity_signup=0x7f050005;
         public static final int title_activity_splash_page=0x7f050003;
+        public static final int title_section1=0x7f050007;
+        public static final int title_section2=0x7f050008;
+        public static final int title_section3=0x7f050009;
     }
     public static final class style {
         /** 
